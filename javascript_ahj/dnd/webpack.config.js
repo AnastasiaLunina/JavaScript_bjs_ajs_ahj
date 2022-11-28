64ae95d7719ba39f56aba5fd280c13b570d46fcf
Anastasia Lunina <lunina.anastasiya@gmail.com>
2022-11-27 20:49:04 -0500

    ahj subdir modified

--- javascript_ahj/dnd/webpack.config.js
+++
@@ -1,8 +0,0 @@
-const LicenseWebpackPlugin = require('license-webpack-plugin').LicenseWebpackPlugin;
-
-module.exports = {
-  plugins: [
-    new LicenseWebpackPlugin()
-  ],
-  mode: 'production',
-};
--- javascript_ahj/dnd/webpack.dev.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const webpack = require('webpack');
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
-
-module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-});
